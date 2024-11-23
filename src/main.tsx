4a8f2733c607d28d4ece3427efaec1f81da85a99
Naveen Cooray <naveen.cooray.nc@gmail.com>
2024-11-23 20:24:51 +0530

    Fix import issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Home from "@/pages/home";
-import "./index.css";
+import "@/styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
